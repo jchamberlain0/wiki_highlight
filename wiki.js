1054b5b0812b4b57462cae66975c2ebf998f41b2
jchamberlain0 <jhc@outlook.com>
2022-04-09 22:03:47 -0400

    loop over paragraphs

--- wiki.js
+++ wiki.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wikipedia Highlighter
 // @namespace    http://wikipedia.org.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://en.wikipedia.org/wiki/*
@@ -10,24 +10,43 @@
 
 (function () {
   'use strict';
-
-  console.log('hello');
-  console.log('hello');
+  console.log('wiki.js v0.1');
 
   //let containerElement = document.getElementsByClassName('mw-parser-output')[0].children[18];
-  let containerElement = document.getElementsByClassName('mw-parser-output')[0].getElementsByTagName('p')[0];
-  let containerElement2 = document.getElementsByClassName('mw-parser-output')[0].getElementsByTagName('p')[0];
+  // let paragraphs = [];
+  // paragraphs.push(;
+  // let containerElement2 = document.getElementsByClassName('mw-parser-output')[0].getElementsByTagName('p')[1];
+  // let containerElement2 = document.getElementsByClassName('mw-parser-output')[0].getElementsByTagName('p')[2];
   // var firstParagraph = document.getElementById('container').getElementsByTagName('p')[0];
-  let originalText = containerElement.innerHTML;
+  // let originalText = containerElement.innerHTML;
 
-  let modifiedText = originalText;
+  // let modifiedText = originalText;
 
-  modifiedText = modifiedText.replace(
-    //' was a ',
-    /(wa|i)s\san?/,
-    '<span style="padding: 0 1.5em 0 1.5em; background: linear-gradient(150deg, #013766 20%, #f62b2c 70%, #313584 100%); border: 0.2em solid white; color: black; border-radius:1em; box-shadow: 0 0 0.5em gray"> was a </span>'
-  );
+  let i = 0;
+  for (i = 0; i < 2; i++) {
+    console.log(i);
+    console.log(i.toString());
 
-  console.log(modifiedText);
-  containerElement.innerHTML = modifiedText;
+    let text = document.getElementsByClassName('mw-parser-output')[0].getElementsByTagName('p')[i];
+
+    let modifiedText = text.innerHTML;
+
+    modifiedText = modifiedText.replace(
+      //' was a ',
+      /(wa|i)s\san?/,
+      `<span \
+      style="\
+      padding: 0 1.5em 0 1.5em;\
+      background: linear-gradient(150deg, #013766 20%, #f62b2c 70%, #313584 100%);\
+      border: 0.2em solid white;\
+      color: black;\
+      border-radius:1em;\
+      box-shadow: 0 0 0.5em gray;\
+    "> was a </span>`
+    );
+    text.innerHTML = modifiedText;
+  }
+
+  // console.log(modifiedText);
+  // containerElement.innerHTML = modifiedText;
 })();
